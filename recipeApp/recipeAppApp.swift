75b42544e0f6df7536f1fdb124f55470c5122625
KambeMisaki <msk.kam12192gmail.com>
2022-05-06 13:42:35 +0900

    Initial Commit

---
+++ recipeApp/recipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeAppApp.swift
+//  recipeApp
+//
+//  Created by かんべみさき on 2022/05/06.
+//
+
+import SwiftUI
+
+@main
+struct recipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
